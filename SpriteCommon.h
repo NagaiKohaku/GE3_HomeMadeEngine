942918a8642c8fb9c3e115cd88e47ec3276028d7
NagaiKohaku <k023g0037@g.neec.ac.jp>
2024-07-12 14:37:14 +0900

    スプライト共通部

--- SpriteCommon.h
+++ SpriteCommon.h
@@ -1,4 +1,9 @@
 #pragma once
+#include "wrl.h"
+#include "externals/DirectXTex/d3dx12.h"
+
+class DirectXCommon;
+
 class SpriteCommon {
 
 public:
@@ -6,4 +11,20 @@ public:
 	static SpriteCommon* GetInstance();
 
 	void Initialize();
+
+	void CommonDrawSetting();
+
+	DirectXCommon* GetDxCommon() const { return dxCommon_; }
+
+private:
+	
+	void CreateRootSignature();
+
+	void CreateGraphicsPipeline();
+
+	DirectXCommon* dxCommon_;
+
+	Microsoft::WRL::ComPtr<ID3D12RootSignature> rootSignature_ = nullptr;
+
+	Microsoft::WRL::ComPtr<ID3D12PipelineState> graphicsPipelineState_ = nullptr;
 };
